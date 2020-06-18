34782f8e624a59e498841b39b037ecde8bcef459
Absalon Blanco <ablanco@cgclatam.com>
2020-06-17 21:33:55 -0600

    pending changes to finish the project

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex-col items-center">
+        <h1 class="text-gray-700 text-6xl">Exchange v1.0.0</h1>
+        <p class="text-gray-600 text-xl text-center">
+            Proyecto para obtener las cotizaciones de las cryptomonedas más importantes
+        </p>
+        <p class="text-gray-600 text-xl text-center">Este proyecto es utilizado en las práctica de vue</p>
+        <router-link to="/" class="mt-5 text-xl text-green-6000 hover:underline">
+        Regresar a la página de inicio</router-link>
+    </div>
+</template>
